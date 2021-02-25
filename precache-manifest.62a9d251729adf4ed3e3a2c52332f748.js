d3788843619dfc5b156e80f2f796b0cddf29504e
Monwar <monwar259@gmail.com>
2021-02-25 12:06:00 +0000

    Updates

---
+++ precache-manifest.62a9d251729adf4ed3e3a2c52332f748.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94772b1b62047477c7228fd43908d629",
+    "url": "/portfolio-react-cp/index.html"
+  },
+  {
+    "revision": "4e481f3bad171c5e2767",
+    "url": "/portfolio-react-cp/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "8dcf20c5756dec285ac7",
+    "url": "/portfolio-react-cp/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "18e5b3cb3da135522f86",
+    "url": "/portfolio-react-cp/static/css/main.b3893663.chunk.css"
+  },
+  {
+    "revision": "fe9438a163e0b1c1c803",
+    "url": "/portfolio-react-cp/static/js/2.ea27f079.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/portfolio-react-cp/static/js/2.ea27f079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e481f3bad171c5e2767",
+    "url": "/portfolio-react-cp/static/js/3.ce52e295.chunk.js"
+  },
+  {
+    "revision": "8dcf20c5756dec285ac7",
+    "url": "/portfolio-react-cp/static/js/4.4af3215e.chunk.js"
+  },
+  {
+    "revision": "18e5b3cb3da135522f86",
+    "url": "/portfolio-react-cp/static/js/main.e4426830.chunk.js"
+  },
+  {
+    "revision": "8a25cbbfe64fdcd867cb",
+    "url": "/portfolio-react-cp/static/js/runtime-main.60832f74.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio-react-cp/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio-react-cp/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/portfolio-react-cp/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio-react-cp/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "53b2ad717fb383dc83d8ec5662e35fa7",
+    "url": "/portfolio-react-cp/static/media/cpLogo.53b2ad71.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio-react-cp/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/portfolio-react-cp/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "35ef5e915631be2c31ccdff55b53db29",
+    "url": "/portfolio-react-cp/static/media/harvardLogo.35ef5e91.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio-react-cp/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/portfolio-react-cp/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio-react-cp/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "3b758ef56d398145542e137daa28e392",
+    "url": "/portfolio-react-cp/static/media/stanfordLogo.3b758ef5.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/portfolio-react-cp/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "53015aad4cf940e4d341c556ac2d8014",
+    "url": "/portfolio-react-cp/static/media/vodafoneLogo.53015aad.png"
+  }
+]);
